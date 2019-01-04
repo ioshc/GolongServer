8e0c0a96fce2bdfa11322ed79c1d02432fbd50bb
ioshc <huangcong520@icloud.com>
2019-01-04 15:30:58 +0800

    Update webServer.go

--- src/webServer.go
+++ src/webServer.go
@@ -16,6 +16,7 @@ func webServerBase() {
 
 	//第一个参数为客户端发起http请求时的接口名，第二个参数是一个func，负责处理这个请求。
 	http.HandleFunc("/login", loginTask)
+	http.HandleFunc("/proxy", proxyTask)
 
 	//服务器要监听的主机地址和端口号
 	err := http.ListenAndServe(":8081", nil)
